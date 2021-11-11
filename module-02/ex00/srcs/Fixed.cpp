7f55e01b848479ec7c747fa57f795d41907e6aa0
public-jun <generalpublicjunjun@gmail.com>
2021-11-12 00:07:26 +0900

    Add: Fixed class

---
+++ module-02/ex00/srcs/Fixed.cpp
@@ -0,0 +1,11 @@
+#include <Fixed.hpp>
+
+Fixed::Fixed() : int_fixed_value_(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+

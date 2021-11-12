e252f5735365d8523ea74f545647db70a2e920f3
public-jun <generalpublicjunjun@gmail.com>
2021-11-12 18:15:15 +0900

    Add: ex01 done

--- module-02/ex01/includes/Fixed.hpp
+++ module-02/ex01/includes/Fixed.hpp
@@ -3,20 +3,28 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 
 class Fixed
 {
 	public:
-		Fixed(const int );
+		Fixed();
+		Fixed(int value);
+		Fixed(float value);
 		Fixed& operator=(const Fixed& rhs);
 		~Fixed();
 		Fixed(const Fixed& rhs);
+
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
 
 	private:
 		int raw_value_;
 		const static int fractional_bits_ = 8;
 };
+
+std::ostream &operator<<(std::ostream& os, const Fixed& fixed);
 
 #endif

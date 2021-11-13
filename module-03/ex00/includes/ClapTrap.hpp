497a98e690c80780b51661339280608911d9127c
public-jun <generalpublicjunjun@gmail.com>
2021-11-14 00:36:29 +0900

    first commit

---
+++ module-03/ex00/includes/ClapTrap.hpp
@@ -0,0 +1,33 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+	public:
+
+	private:
+		std::string name_;
+		unsigned int hitpoints_;
+		unsigned int energy_points_;
+		unsigned int attack_damage_;
+
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap& other);
+		ClapTrap &operator=(const ClapTrap& other);
+		~ClapTrap();
+
+		const std::string &get_name(void) const;
+		unsigned int get_hitpoints(void) const;
+		unsigned int get_energy_points(void) const;
+		unsigned int get_attak_damage(void) const;
+
+		void attack(std::string const &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
+
+#endif

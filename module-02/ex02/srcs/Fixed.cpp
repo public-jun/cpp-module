5c660afec414087f55faf7d290a0d5265766953c
public-jun <generalpublicjunjun@gmail.com>
2021-11-13 20:03:00 +0900

    Fix:  ex02

--- module-02/ex02/srcs/Fixed.cpp
+++ module-02/ex02/srcs/Fixed.cpp
@@ -35,37 +35,10 @@ void Fixed::setRawBits(int const raw) {
 
 float Fixed::toFloat(void) const {
 	return (static_cast<float>(this->raw_value_) / (1 << fractional_bits_));
-	// int value_int = raw_value_;
-	// value_int >>= fractional_bits_;
-	// float value = value_int;
-	// float value_to_add = 1;
-	// for (int i = 0; i < fractional_bits_; ++i)
-	// {
-	// 	value_to_add /= 2;
-	// 	int mask = 1;
-	// 	mask <<= fractional_bits_ - i - 1;
-	// 	if (mask & raw_value_)
-	// 	{
-	// 		value += value_to_add;
-	// 	}
-	// }
-	// return value;
 }
 
 int Fixed::toInt(void) const {
 	return (this->raw_value_ >> fractional_bits_);
-	// int value = raw_value_;
-	// value >>= fractional_bits_;
-	// if (value < 0)
-	// {
-	// 	int value_compair = value;
-	// 	value_compair <<= fractional_bits_;
-	// 	if (raw_value_ != value_compair)
-	// 	{
-	// 		value += 1;
-	// 	}
-	// }
-	// return value;
 }
 
 

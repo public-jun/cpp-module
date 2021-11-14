89cdf4acf3834771a4797db508a312031ac803e7
public-jun <generalpublicjunjun@gmail.com>
2021-11-14 20:13:21 +0900

    ex03 first commit

---
+++ module-03/ex03/includes/ScavTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap& other);
+		ScavTrap &operator=(const ScavTrap &other);
+		virtual ~ScavTrap();
+
+		void attack(std::string const &target);
+		void guardGate();
+};
+
+#endif
---
+++ module-03/ex03/srcs/main.cpp
@@ -0,0 +1,37 @@
+#include <FragTrap.hpp>
+
+void print_profile(const FragTrap &a)
+{
+	std::cout << "\n>>>>>>>>>>>>>>>>>>>" << std::endl;
+	std::cout << "name :" << a.get_name() << std::endl;
+	std::cout << "hitpoints :" << a.get_hitpoints() << std::endl;
+	std::cout << "energy_points :" << a.get_energy_points() << std::endl;
+	std::cout << "attack_damage :" << a.get_attak_damage() << std::endl;
+	std::cout << "<<<<<<<<<<<<<<<<<<<\n" << std::endl;
+}
+
+int main(void)
+{
+	FragTrap no_name;
+
+	print_profile(no_name);
+	no_name.takeDamage(3);
+
+	FragTrap bob("bob");
+	print_profile(bob);
+	bob.takeDamage(3);
+
+	FragTrap bob2(bob);
+	print_profile(bob2);
+	bob.takeDamage(10);
+
+	FragTrap bob3;
+	bob3 = bob;
+	print_profile(bob3);
+
+	bob.attack("tokyo");
+	bob.beRepaired(30);
+	bob.highFivesGuys();
+
+	std::cout << std::endl;
+}

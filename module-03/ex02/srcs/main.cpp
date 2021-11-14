cba57411447a8a6b153968228f4c88a5f80eb5c9
public-jun <generalpublicjunjun@gmail.com>
2021-11-14 18:24:03 +0900

    Add: ex02 first commit

---
+++ module-03/ex02/srcs/main.cpp
@@ -0,0 +1,37 @@
+#include <ScavTrap.hpp>
+
+void print_profile(const ScavTrap &a)
+{
+	std::cout << "\n>>>>>>>>>>>>>>>>>>>" << std::endl;
+	std::cout << "name :" << a.get_name() << std::endl;
+	std::cout << "hitpoints :" << a.get_hitpoints() << std::endl;
+	std::cout << "energy_points :" << a.get_energy_points() << std::endl;
+	std::cout << "attack_damage :" << a.get_attak_damage() << std::endl;
+	std::cout << "<<<<<<<<<<<<<<<<<<<\n" << std::endl;
+}
+
+int main(void)
+{
+	ScavTrap no_name;
+
+	print_profile(no_name);
+	no_name.takeDamage(3);
+
+	ScavTrap bob("bob");
+	print_profile(bob);
+	bob.takeDamage(3);
+
+	ScavTrap bob2(bob);
+	print_profile(bob2);
+	bob.takeDamage(10);
+
+	ScavTrap bob3;
+	bob3 = bob;
+	print_profile(bob3);
+
+	bob.attack("tokyo");
+	bob.beRepaired(30);
+	bob.guardGate();
+
+	std::cout << std::endl;
+}

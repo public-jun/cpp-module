584845e39c5ea33e63d3f382e8dca09703993708
public-jun <generalpublicjunjun@gmail.com>
2021-11-15 22:39:48 +0900

    Add: Cat class

--- module-04/ex00/srcs/main.cpp
+++ module-04/ex00/srcs/main.cpp
@@ -1,5 +1,6 @@
 #include <Animal.hpp>
 #include <Dog.hpp>
+#include <Cat.hpp>
 #include <string>
 #include <iostream>
 
@@ -7,15 +8,16 @@ int main(void)
 {
 	const Animal *meta = new Animal();
 	const Animal *j = new Dog();
-	// const Animal *i = new Cat();
+	const Animal *i = new Cat();
 	std::cout << j->getType() << " " << std::endl;
-	// std::cout << i->getType() << " " << std::endl;
-	// i->makeSound(); //will output the cat sound!
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
 
 	delete meta;
 	delete j;
+	delete i;
 
 	// Animal *animal1 = new Animal();
 

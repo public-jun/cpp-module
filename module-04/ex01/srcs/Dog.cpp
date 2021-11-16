6fb4fd68a327725ab2e195e1a2fe51ce5bab95d6
public-jun <generalpublicjunjun@gmail.com>
2021-11-16 15:17:05 +0900

    update: Dog class

--- module-04/ex01/srcs/Dog.cpp
+++ module-04/ex01/srcs/Dog.cpp
@@ -4,14 +4,23 @@
 
 Dog::Dog()
 {
-	type_ = "Dog";
 	std::cout << GREEN
 			  << "Dog default constructor called"
 			  << END
 			  << std::endl;
+	type_ = "Dog";
+	try
+	{
+		brain_ = new Brain();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		throw;
+	}
 }
 
-Dog::Dog(const Dog &other)
+Dog::Dog(const Dog &other) : brain_(new Brain())
 {
 	std::cout << GREEN
 			  << "Dog copy constructor called"
@@ -22,12 +31,25 @@ Dog::Dog(const Dog &other)
 
 Dog &Dog::operator=(const Dog &other)
 {
-	this->type_ = other.getType();
+	std::cout << GREEN
+			  << "Dog Assignnation operator called"
+			  << END
+			  << std::endl;
+	if (this != &other)
+	{
+		type_ = other.getType();
+
+		Brain *p = new Brain();
+		*p = *(other.brain_);
+		delete brain_;
+		brain_ = p;
+	}
 	return (*this);
 }
 
 Dog::~Dog()
 {
+	delete brain_;
 	std::cout << GREEN
 			  << "Dog destructor called"
 			  << END

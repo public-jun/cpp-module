afc35a87eb8bc6b8c04e2851f6d234d611eeecfa
public-jun <generalpublicjunjun@gmail.com>
2021-11-16 16:28:36 +0900

    Add: ex01 done

--- module-04/ex01/includes/Cat.hpp
+++ module-04/ex01/includes/Cat.hpp
@@ -2,6 +2,7 @@
 #define CAT_HPP
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 #include <string>
 
 class Cat : public Animal
@@ -13,6 +14,13 @@ class Cat : public Animal
 		virtual ~Cat();
 
 		virtual void makeSound() const;
+
+		const Brain *get_brain() const;
+		const std::string &get_brain_idea(int index) const;
+		void set_brain_idea(int index, std::string idea);
+
+	private:
+		Brain *brain_;
 };
 
 #endif

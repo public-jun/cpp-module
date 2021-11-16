b87a38662426044067af67768856e0f5326aa65d
public-jun <generalpublicjunjun@gmail.com>
2021-11-16 23:39:39 +0900

    Add: AMateria abstract class

---
+++ module-04/ex03/srcs/AMateria.cpp
@@ -0,0 +1,38 @@
+#include <AMateria.hpp>
+#include <color.hpp>
+#include <iostream>
+
+AMateria::AMateria()
+{
+}
+
+AMateria::AMateria(std::string const &type) :
+	type_(type)
+{
+}
+
+AMateria::AMateria(const AMateria &other)
+{
+	*this = other;
+}
+
+AMateria &AMateria::operator=(const AMateria &other)
+{
+	if (this != &other)
+	{
+		std::cout << BOLD
+				  << "copying the type doesn’t make sense... "
+				  << END << std::endl;
+		type_ = "";
+	}
+	return (*this);
+}
+
+AMateria::~AMateria()
+{
+}
+
+std::string const &AMateria::getType() const
+{
+	return (type_);
+}

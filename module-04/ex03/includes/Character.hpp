32b649e6ca705a24df1e0c09d78b6f9a246d3c4d
public-jun <generalpublicjunjun@gmail.com>
2021-11-17 22:03:52 +0900

    Add: Character class

---
+++ module-04/ex03/includes/Character.hpp
@@ -0,0 +1,36 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include <ICharacter.hpp>
+#include <AMateria.hpp>
+#include <string>
+
+#define NUM_MATERIA 4
+
+class Character : public ICharacter
+{
+	private:
+		std::string name_;
+		AMateria *inventory_[NUM_MATERIA];
+
+		Character();
+
+	public:
+		Character(std::string name);
+		Character(const Character &other);
+		Character &operator=(const Character &other);
+		virtual ~Character();
+
+		bool is_valid_index(int idx) const;
+
+		virtual std::string const &getName() const;
+		const AMateria *getMateria(int idx) const;
+
+		virtual void equip(AMateria *m);
+		virtual void unequip(int idx);
+		virtual void use(int idx, ICharacter &target);
+
+
+}
+
+#endif

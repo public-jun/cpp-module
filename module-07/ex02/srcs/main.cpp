3272cd485bce4ecf8316018a194a658b3fd493f8
public-jun <generalpublicjunjun@gmail.com>
2021-11-23 15:53:47 +0900

    Add: color

--- module-07/ex02/srcs/main.cpp
+++ module-07/ex02/srcs/main.cpp
@@ -1,13 +1,41 @@
 #include <Array.hpp>
+#include <color.hpp>
 #include <string>
 #include <iostream>
 
+void printTitle(std::string title)
+{
+	std::cout << "\n"
+			  << BACK_GRAY << title << END << "\n"
+			  << std::endl;
+}
+
 int main(void)
 {
-	Array<int> array(10);
-	Array<int> array2 = Array<int>();
-	Array<int> array3 = array;
+	printTitle("----CONSTRUCTOR----");
+	{
+		Array<int> array_empty;
+		Array<int> array_42(42);
 
-	array2 = array;
-	// array = array;
+		Array<int> assign_array_a(10);
+		Array<int> assign_array_b(20);
+
+		assign_array_a = assign_array_b;
+
+		Array<int> array_copy_empty = array_empty;
+	}
+
+	{
+		const Array<int> array_empty;
+		const Array<int> array_42(42);
+
+		const Array<int> assign_array_a(10);
+		const Array<int> assign_array_b(20);
+
+		// assign_array_a = assign_array_b;
+
+		const Array<int> array_copy_empty = array_empty;
+	}
+
+	printTitle("----DESTRUCTOR----");
 }

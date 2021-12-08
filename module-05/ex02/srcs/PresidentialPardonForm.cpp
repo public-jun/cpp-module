80541bb6582bb8eb66f24ea184c589c03e94133e
public-jun <generalpublicjunjun@gmail.com>
2021-12-08 15:39:07 +0900

    shrubbery done

---
+++ module-05/ex02/srcs/PresidentialPardonForm.cpp
@@ -0,0 +1,32 @@
+#include <PresidentialPardonForm.hpp>
+#include <color.hpp>
+
+const std::string PresidentialPardonForm::presidential_name_ = "PresidentialPardon";
+
+PresidentialPardonForm::PresidentialPardonForm()
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string &target) :
+	Form(presidential_name_, target, require_sign_to_grade_, require_exe_to_grade_)
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &other)
+{
+	*this = other;
+}
+
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &other)
+{
+	(void) other;
+	return (*this);
+}
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+}
+
+void PresidentialPardonForm::action() const
+{
+	std::cout << GetTarget() << " has been pardoned by Zafod Beeblebrox" << std::endl;
+}

29acd949b9196bd3d2075ecfe0d085eb4cf9b1b9
public-jun <generalpublicjunjun@gmail.com>
2021-12-10 19:15:48 +0900

    Add: check Type

--- module-06/ex00/srcs/Scalar.cpp
+++ module-06/ex00/srcs/Scalar.cpp
@@ -1,98 +1,65 @@
 #include <Scalar.hpp>
-#include <sstream>
-#include <limits>
-#include <iomanip>
-#include <iostream>
 
-Scalar::Scalar(std::string literal_value) :
-	literal_value_(literal_value) {}
+Scalar::Scalar() {}
 
-Scalar::Scalar(const Scalar &other) :
-	literal_value_(other.GetLiteralValue()) {}
+Scalar::Scalar(const std::string &literal_value) :
+	literal_value_(literal_value), type_(kDef) {}
 
-Scalar &Scalar::operator=(const Scalar &other)
+Scalar::Scalar(const Scalar &other)
 {
-	(void)other;
+	*this = other;
+}
+
+Scalar &Scalar::operator=(const Scalar &)
+{
 	return (*this);
 }
 
 Scalar::~Scalar() {}
 
-const std::string Scalar::GetLiteralValue() const
+const std::string Scalar::getLiteralValue() const
 {
 	return (literal_value_);
 }
 
-// bool Scalar::IsNumeric() const
-// {
-// 	std::string literal_value = this->GetLiteralValue();
-// 	if (literal_value)
-// 	{
-// 		throw (ImpossibleException("impossible"));
-// 	}
+// 型判定
+// char , int , float, double, invalid_type
+// -inff +inff nanf , -inf +inf nan
 
-// 	return (true);
-// }
-
-// void Scalar::LiteralToStore()
-// {
-// 	//空かどうか
-// 	if (literal_value_.empty())
-// 	{
-// 		throw (ImpossibleException("impossible"));
-// 	}
-// 	//正しいformatかどうか
-// }
-
-
-
-void Scalar::ToChar() const
+static void testCheckType(Scalar::Types type)
 {
-	double res;
-
-	try
-	{
-		// valueが適切かどうか判定
-		// if (!IsNumeric())
-		// {
-		// 	throw (ImpossibleException("impossible"));
-		// }
-		// res = std::numeric_limits<int>::max();
-		std::string str = "42.0f";
-		std::istringstream iss(str);
-		iss >> res;
-		// if ((res >= 0 && res < 32) || res == 127)
-		// {
-		// 	throw (NonDisplayableException("Non displayable"));
-		// }
-		// else if (res < 32 || res > 126)
-		// {
-		// 	throw (ImpossibleException("impossible"));
-		// }
-		// std::cout << "char: " << res << std::endl;
-		std::cout << "int: " << static_cast<int>(res) << std::endl;
-		std::cout << "float: " << static_cast<float>(res) << "f" << std::endl;
-		std::cout << "double: " << static_cast<double>(res) << std::endl;
-	}
-	catch(std::exception& e)
-	{
-		std::cerr << "char: " << e.what() << '\n';
-	}
-
+	if (type == Scalar::kNinf)
+		std::cout << "-inf" << std::endl;
+	else if (type == Scalar::kPinf)
+		std::cout << "+inf" << std::endl;
+	else if (type == Scalar::kNan)
+		std::cout << "nan" << std::endl;
+	else if (type == Scalar::kChar)
+		std::cout << "char" << std::endl;
+	else
+		std::cout << "default" << std::endl;
 }
 
-Scalar::ImpossibleException::ImpossibleException(const char *msg) :
-	msg_(msg) {}
-
-const char *Scalar::ImpossibleException::what() const throw()
+Scalar::Types Scalar::checkType()
 {
-	return (msg_);
+	std::string literal_value = getLiteralValue();
+	if (literal_value == "-inff" || literal_value == "-inf")
+		return (kNinf);
+	else if (literal_value == "+inff" || literal_value == "+inf")
+		return (kPinf);
+	else if (literal_value == "nanf" || literal_value == "nan")
+		return (kNan);
+	// char
+	else if (literal_value.length() == 1 && std::isprint(literal_value[0]) && !std::isdigit(literal_value[0]))
+		return (kChar);
+	// int
+	// float
+	// double
+	return (kDef);
 }
 
-Scalar::NonDisplayableException::NonDisplayableException(const char *msg) :
-	msg_(msg) {}
-
-const char *Scalar::NonDisplayableException::what() const throw()
+void Scalar::convert()
 {
-	return (msg_);
+	type_ = checkType();
+	testCheckType(type_);
 }

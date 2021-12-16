2659e79ae9d9b0bfff96c1f75cb5ca3925964b15
public-jun <generalpublicjunjun@gmail.com>
2021-12-16 19:10:26 +0900

    Add: easyfind

--- module-08/ex00/srcs/main.cpp
+++ module-08/ex00/srcs/main.cpp
@@ -1,8 +1,35 @@
 #include <easyfind.hpp>
 #include <iostream>
+#include <iomanip>
+#include <vector>
+
+void testEasyFind(std::vector<int> &v, int val)
+{
+	std::cout << std::setw(5) << val << ": " <<std::flush;
+	try
+	{
+		std::cout << *easyfind(v, val) << std::flush;
+		std::cout << " find" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
 
 int main(void)
 {
-	std::cout << "hello world" << std::endl;
+	const int a[] = {1, 22, 33, 44, 55};
+
+	std::vector<int> v(a, a + 5);
+	for (std::size_t i = 0; i < v.size(); ++i)
+	{
+		std::cout << std::setw(3) << "[" << i << "]: " << v.at(i) << std::endl;
+	}
+
+
+	testEasyFind(v, 66);
+	testEasyFind(v, 1);
+
 	return 0;
 }
